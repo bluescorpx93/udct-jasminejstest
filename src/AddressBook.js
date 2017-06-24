ac4142d84950ffc1da7f85def0aac45244ff0de9
Toufiq <burningraven06@gmail.com>
2017-06-24 22:50:08 +0600

    Pushing Code, first Commit

---
+++ src/AddressBook.js
@@ -0,0 +1,20 @@
+function AddressBook(){
+	this.contacts = [];
+	this.initialContactsRetrieval = false;
+}
+AddressBook.prototype.getInitialContacts = function(cb){
+	var self = this;
+	setTimeout(function(){
+		self.initialContactsRetrieval = true;
+		if (cb){ return cb()};
+	}, 3);
+}
+AddressBook.prototype.addContact = function(contact){
+	this.contacts.push(contact);
+}
+AddressBook.prototype.getContact = function(index){
+	return this.contacts[index];
+}	
+AddressBook.prototype.deleteContact = function(index){
+	this.contacts.splice(index, 1);
+}
